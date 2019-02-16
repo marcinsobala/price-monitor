a256d72d56778740008ca7f29b81d49c6d16298c
Marcin Sobala <marcin.sobala09@gmail.com>
2019-02-16 17:23:25 +0100

    price, currency, name scrape for new prices and edited prices - for 2 shops

--- PriceMonitor/tracked_prices/views.py
+++ PriceMonitor/tracked_prices/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import TrackedPrice
 from .forms import PriceForm
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from . scrape import name_price_currency
+
 
 def tracked_prices_list(request):
     prices = TrackedPrice.objects.all().order_by('-last_checked_date')
@@ -13,14 +16,13 @@ def price_detail(request, pk):
     return render(request, 'tracked_prices/price_detail.html', {'price': price})
 
 
+@login_required
 def price_new(request):
     if request.method == "POST":
         form = PriceForm(request.POST)
         if form.is_valid():
             price = form.save(commit=False)
-            price.name = "Some name"
-            price.current = 0
-            price.currency = "PLN"
+            price.name, price.current, price.currency = name_price_currency(price.url)
             price.user = request.user
             price.last_checked_date = timezone.now()
             price.save()
@@ -30,3 +32,26 @@ def price_new(request):
 
     return render(request, 'tracked_prices/price_new.html', {'form': form})
 
+
+@login_required
+def price_edit(request, pk):
+    price = get_object_or_404(TrackedPrice, pk=pk)
+    if request.method == "POST":
+        form = PriceForm(request.POST, instance=price)
+        if form.is_valid():
+            price = form.save(commit=False)
+            price.name, price.current, price.currency = name_price_currency(price.url)
+            price.last_checked_date = timezone.now()
+            price.save()
+            return redirect('price_detail', pk=price.pk)
+    else:
+        form = PriceForm(instance=price)
+
+    return render(request, 'tracked_prices/price_edit.html', {'form': form})
+
+
+@login_required
+def price_remove(request, pk):
+    price = get_object_or_404(TrackedPrice, pk=pk)
+    price.delete()
+    return redirect('tracked_prices_list')
