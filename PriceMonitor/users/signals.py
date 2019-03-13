f69cf9dc456dddfc21e65fd4d289355d3c92c695
Marcin Sobala <marcin.sobala09@gmail.com>
2019-03-13 20:45:30 +0100

    Added pretty icons to navbar items and replaced logout view with a message on homepage

--- PriceMonitor/users/signals.py
+++ PriceMonitor/users/signals.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.signals import user_logged_in
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 from django.contrib import messages
 from django.shortcuts import render, redirect
@@ -9,6 +9,8 @@ from django.shortcuts import render, redirect
 def on_user_logged_in(sender, request, user, **kwargs):
     messages.success(request, f"Logowanie przebiegło pomyślnie. Witaj, {user}!")
 
-
+@receiver(user_logged_out)
+def on_user_logged_out(sender, request, user, **kwargs):
+    messages.success(request, f"Wylogowano z serwisu.")
 
 
