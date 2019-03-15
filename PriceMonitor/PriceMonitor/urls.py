da2d9cf0f641e7e39c06e25ede3f9c0b4b8fddb6
Marcin Sobala <marcin.sobala09@gmail.com>
2019-03-15 20:59:32 +0100

    added password reset views and urls, modified their templates slightly and created custom email message and subject

--- PriceMonitor/PriceMonitor/urls.py
+++ PriceMonitor/PriceMonitor/urls.py
@@ -9,5 +9,27 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
-    path('', include('tracked_prices.urls')),
+    path('password-reset/',
+         views.PasswordResetView.as_view(
+             template_name='users/password_reset.html',
+             email_template_name='users/password_reset_email.html',
+             subject_template_name='users/password_reset_subject.txt'
+         ),
+         name='password_reset'),
+    path('password-reset/done',
+         views.PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>',
+         views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         views.PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+    path('', include('tracked_prices.urls'))
 ]
