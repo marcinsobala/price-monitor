8a20d2d26d7ae3c61d18a2cea84f412ec8136122
Marcin Sobala <marcin.sobala09@gmail.com>
2019-09-18 21:08:39 +0200

    Added border to table header in prices table and css styling for name and col header links.

--- PriceMonitor/tracked_prices/tables.py
+++ PriceMonitor/tracked_prices/tables.py
@@ -2,6 +2,7 @@ import django_tables2 as tables
 from django.utils.html import format_html
 from .models import TrackedPrice
 
+
 class PriceTable(tables.Table):
 
     class Meta:
@@ -10,12 +11,14 @@ class PriceTable(tables.Table):
         fields = ('name', 'when_inform', 'current', 'last_checked_date')
         attrs = {'class': 'table table-hover table-responsive'}
 
-    name = tables.Column("Nazwa")
+    name = tables.Column("Nazwa", attrs={'th': {'class': "col_header"}})
 
     def render_name(self, record):
-        return format_html(f"<a href=\"{record.url}\">{record.name}</a>")
+        return format_html(f"<a href=\"{record.url}\" class=\"price_name\">{record.name}</a>")
 
-    when_inform = tables.Column("Wyślij powiadomienie", attrs={'td': {'style': 'width: 17%'}})
+    when_inform = tables.Column("Wyślij powiadomienie",
+                                attrs={'td': {'style': 'width: 17%'},
+                                       'th': {'class': "col_header"}})
 
     def render_when_inform(self, record):
         if record.when_inform == "A":
@@ -27,22 +30,33 @@ class PriceTable(tables.Table):
 
         return message
 
-    current = tables.Column('Obecna cena', attrs={'td': {'style': 'width: 10%'}})
+    current = tables.Column('Obecna cena',
+                            attrs={'td': {'style': 'width: 10%'},
+                                   'th': {'class': "col_header"}})
 
     def render_current(self, record):
         return f"{record.current} {record.currency}"
 
-    last_checked_date = tables.Column ("Ostatnio sprawdzono")
+    last_checked_date = tables.Column("Ostatnio sprawdzono",
+                                      attrs={'th': {'class': "col_header"}})
 
     def render_last_checked_date(self, value):
         return value.strftime("%d.%m.%y")
 
-    edit = tables.Column('Edytuj', linkify=('price_edit', (tables.A("id"), )), orderable=False, empty_values=())
+    edit = tables.Column('Edytuj',
+                         linkify=('price_edit', (tables.A("id"), )),
+                         orderable=False,
+                         empty_values=(),
+                         attrs={'th': {'class': "col_header"}})
 
     def render_edit(self):
         return format_html("<i id = \"ihover\" class=\"fas fa-edit text-muted\"></i>")
 
-    delete = tables.Column('Usuń', linkify=('price_remove', (tables.A("id"), )), orderable=False, empty_values=())
+    delete = tables.Column('Usuń',
+                           linkify=('price_remove', (tables.A("id"), )),
+                           orderable=False,
+                           empty_values=(),
+                           attrs={'th': {'class': "col_header"}})
 
     def render_delete(self):
         return format_html("<i id=\"ihover\" class=\"fas fa-trash-alt text-danger\"></i>")
